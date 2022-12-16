05d2f43bfd46704b620696c8de1b900c0d242aa6
Henrique <riquependragon@gmail.com>
2022-12-16 04:47:06 -0300

    Ajeitando css da repetição

--- public_html/selecao/jogos/repeticao/script/gameScript.js
+++ public_html/selecao/jogos/repeticao/script/gameScript.js
@@ -1,11 +1,11 @@
 /** CONSTANTES DO SCRIPT **/
 
 // IDs dos containers
-const divSequencia = 'container-nucleo';
-const divOpcoes = 'container-formas';
-const divCaixa = 'container-sequencia'; //container-resposta
-const textNumeroFase = 'textbox-numero-fase';
-const divEstrelas = 'container-estrelas';
+const divSequencia = 'container_nucleo';
+const divOpcoes = 'container_formas';
+const divCaixa = 'container_sequencia'; //container-resposta
+const textNumeroFase = 'textbox_numero_fase';
+const divEstrelas = 'conquistas_conteiner';
 
 const anosEnum = Object.freeze({
 	"Primeiro ano": 1,
